2cd106f78a0e08795b1c8baa6c0a25d1b9bdd2de
shafat-96 <animeexost61@gmail.com>
2025-07-17 00:34:26 +0600

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -19,7 +19,7 @@ mod templates;
 static ALLOWED_ORIGINS: Lazy<[&str; 3]> = Lazy::new(|| [
     "http://localhost:5173",
     "http://localhost:3000",
-    "http://aniwave.at",
+    "https://rust.madaraverse.online",
 ]);
 
 // Reqwest client pool
